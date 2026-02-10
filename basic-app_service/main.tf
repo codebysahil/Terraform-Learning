d3b93950d7e56977b0b567feb3583e2a7681bb10
Sahil Satija <96464952+codebysahil@users.noreply.github.com>
2026-02-10 18:55:42 +0530

    Add Azure resources for basic app deployment

---
+++ basic-app_service/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "basic-app-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "B1"
+}
+
+
+resource "azurerm_linux_web_app" "web" {
+  name                = var.app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    always_on = false
+  }
+}
