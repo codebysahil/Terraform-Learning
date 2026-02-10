6941e4f49c06dbb1af16f63586e7b090626fa0c2
Sahil Satija <96464952+codebysahil@users.noreply.github.com>
2026-02-10 19:13:11 +0530

    Create main.tf

---
+++ multipe-web-app-for-each/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = local.app_service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  os_type  = "Linux"
+  sku_name = "F1"
+}
+
+
+resource "azurerm_linux_web_app" "webapps" {
+  for_each            = local.web_apps
+  name                = each.value.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    always_on = false
+  }
+}
