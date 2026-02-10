43de112da618e6b174665c69dee7bb25f92219a2
Sahil Satija <96464952+codebysahil@users.noreply.github.com>
2026-02-10 19:14:07 +0530

    Add variables for location, resource group, and environment

---
+++ multipe-web-app-for-each/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  type    = string
+  default = "West Europe"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "rg-basic-app"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
